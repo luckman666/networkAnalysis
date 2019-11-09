82eeb044f7d89b4492b57628e1ed3555830a3e56
boge <b8_yang@163.com>
2019-11-09 15:43:49 +0800

    测试完成

--- netAnalysis.sh
+++ netAnalysis.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
- 
-#write by zhumaohai(admin#centos.bz)
-#author blog: www.centos.bz
- 
+
  
 #显示菜单(单选)
 display_menu(){
@@ -11,7 +8,7 @@ local prompt="which ${soft} you'd select: "
 eval local arr=(\${${soft}_arr[@]})
 while true
 do
-    echo -e "#################### ${soft} setting ####################\n\n"
+    echo -ne "#################### ${soft} setting ####################\n\n"
     for ((i=1;i<=${#arr[@]};i++ )); do echo -e "$i) ${arr[$i-1]}"; done
     echo
     read -p "${prompt}" $soft
@@ -37,12 +34,12 @@ bit_to_human_readable(){
         if [[ ${trafficValue%.*} -gt 922 ]];then
             #conv to Mb
             trafficValue=`awk -v value=$trafficValue 'BEGIN{printf "%0.1f",value/1024}'`
-            echo -e "${trafficValue}Mb \n"
+            echo -ne "${trafficValue}Mb \n"
         else
-            echo -e "${trafficValue}Kb \n"
+            echo -ne "${trafficValue}Kb \n"
         fi
     else
-        echo -e "${trafficValue}b \n"
+        echo -ne "${trafficValue}b \n"
     fi
 }
  
@@ -59,7 +56,7 @@ check_package_manager(){
     elif cat /proc/version | grep -q -E -i "centos|red hat|redhat";then
         systemPackage='yum'
     else
-        echo -e "unkonw \n"
+        echo -ne "unkonw \n"
     fi
  
     if [ "$manager" == "$systemPackage" ];then
@@ -142,7 +139,7 @@ trafficAndConnectionOverview(){
         eth=$nic
     fi
  
-    echo -e "please wait for 10s to generate network data... \n"
+    echo -ne "please wait for 10s to generate network data... \n"
     echo
     #当前流量值
     local traffic_be=(`awk -v eth=$eth -F'[: ]+' '{if ($0 ~eth){print $3,$11}}' /proc/net/dev`)
@@ -157,9 +154,9 @@ trafficAndConnectionOverview(){
     #打印10s平均速率
     local eth_in=$(( (${traffic_af[0]}-${traffic_be[0]})*8/10 ))
     local eth_out=$(( (${traffic_af[1]}-${traffic_be[1]})*8/10 ))
-    echo -e "\033[32mnetwork device $eth average traffic in 10s: \033[0m \n"
-    echo -e "$eth Receive: $(bit_to_human_readable $eth_in)/s \n"
-    echo -e "$eth Transmit: $(bit_to_human_readable $eth_out)/s \n"
+    echo -ne "\033[32mnetwork device $eth average traffic in 10s: \033[0m \n"
+    echo -ne "$eth Receive: $(bit_to_human_readable $eth_in)/s \n"
+    echo -ne "$eth Transmit: $(bit_to_human_readable $eth_out)/s \n"
     echo
 
     local regTcpdump=$(ifconfig | grep -A 1 $eth | awk -F'[: ]+' '$0~/inet addr:/{printf $4"|"}' | sed -e 's/|$//' -e 's/^/(/' -e 's/$/)\\\\\.[0-9]+:/')
@@ -177,64 +174,64 @@ trafficAndConnectionOverview(){
     awk '{len=$NF;sub(/\)/,"",len);getline;print $0,len}' /tmp/tcpdump_temp2 > /tmp/tcpdump
 
     #统计每个端口在10s内的平均流量
-    echo -e "\033[32maverage traffic in 10s base on server port: \033[0m \n"
+    echo -ne "\033[32maverage traffic in 10s base on server port: \033[0m \n"
     awk -F'[ .:]+' -v regTcpdump=$regTcpdump '{if ($0 ~ regTcpdump){line="clients > "$8"."$9"."$10"."$11":"$12}else{line=$2"."$3"."$4"."$5":"$6" > clients"};sum[line]+=$NF*8/10}END{for (line in sum){printf "%s %d\n",line,sum[line]}}' /tmp/tcpdump | \
     sort -k 4 -nr | head -n 10 | while read a b c d;do
         echo -e "$a $b $c $(bit_to_human_readable $d)/s \n"
     done
     echo -ne "\033[11A \n"
     echo -ne "\033[50C \n"
-    echo -e "\033[32maverage traffic in 10s base on client port: \033[0m \n"
+    echo -ne "\033[32maverage traffic in 10s base on client port: \033[0m \n"
     awk -F'[ .:]+' -v regTcpdump=$regTcpdump '{if ($0 ~ regTcpdump){line=$2"."$3"."$4"."$5":"$6" > server"}else{line="server > "$8"."$9"."$10"."$11":"$12};sum[line]+=$NF*8/10}END{for (line in sum){printf "%s %d\n",line,sum[line]}}' /tmp/tcpdump | \
     sort -k 4 -nr | head -n 10 | while read a b c d;do
             echo -ne "\033[50C \n"
-            echo -e "$a $b $c $(bit_to_human_readable $d)/s \n"
+            echo -ne "$a $b $c $(bit_to_human_readable $d)/s \n"
     done   
         
     echo
 
     #统计在10s内占用带宽最大的前10个ip
-    echo -e "\033[32mtop 10 ip average traffic in 10s base on server: \033[0m \n"
+    echo -ne "\033[32mtop 10 ip average traffic in 10s base on server: \033[0m \n"
     awk -F'[ .:]+' -v regTcpdump=$regTcpdump '{if ($0 ~ regTcpdump){line=$2"."$3"."$4"."$5" > "$8"."$9"."$10"."$11":"$12}else{line=$2"."$3"."$4"."$5":"$6" > "$8"."$9"."$10"."$11};sum[line]+=$NF*8/10}END{for (line in sum){printf "%s %d\n",line,sum[line]}}' /tmp/tcpdump | \
     sort -k 4 -nr | head -n 10 | while read a b c d;do
-        echo -e "$a $b $c $(bit_to_human_readable $d)/s \n"
+        echo -ne "$a $b $c $(bit_to_human_readable $d)/s \n"
     done
     echo -ne "\033[11A \n"
     echo -ne "\033[50C \n"
-    echo -e "\033[32mtop 10 ip average traffic in 10s base on client: \033[0m \n"
+    echo -ne "\033[32mtop 10 ip average traffic in 10s base on client: \033[0m \n"
     awk -F'[ .:]+' -v regTcpdump=$regTcpdump '{if ($0 ~ regTcpdump){line=$2"."$3"."$4"."$5":"$6" > "$8"."$9"."$10"."$11}else{line=$2"."$3"."$4"."$5" > "$8"."$9"."$10"."$11":"$12};sum[line]+=$NF*8/10}END{for (line in sum){printf "%s %d\n",line,sum[line]}}' /tmp/tcpdump | \
     sort -k 4 -nr | head -n 10 | while read a b c d;do
         echo -ne "\033[50C \n"
-        echo -e "$a $b $c $(bit_to_human_readable $d)/s \n"
+        echo -ne "$a $b $c $(bit_to_human_readable $d)/s \n"
     done 
 
     echo
     #统计连接状态
     local regSS=$(ifconfig | grep -A 1 $eth | awk -F'[: ]+' '$0~/inet addr:/{printf $4"|"}' | sed -e 's/|$//')
     ss -an | grep -v -E "LISTEN|UNCONN" | grep -E "$regSS" > /tmp/ss
-    echo -e "\033[32mconnection state count: \033[0m \n"
+    echo -ne "\033[32mconnection state count: \033[0m \n"
     awk 'NR>1{sum[$(NF-4)]+=1}END{for (state in sum){print state,sum[state]}}' /tmp/ss | sort -k 2 -nr
     echo
     #统计各端口连接状态
-    echo -e "\033[32mconnection state count by port base on server: \033[0m \n"
+    echo -ne "\033[32mconnection state count by port base on server: \033[0m \n"
     awk 'NR>1{sum[$(NF-4),$(NF-1)]+=1}END{for (key in sum){split(key,subkey,SUBSEP);print subkey[1],subkey[2],sum[subkey[1],subkey[2]]}}' /tmp/ss | sort -k 3 -nr | head -n 10   
     echo -ne "\033[11A \n"
     echo -ne "\033[50C \n"
-    echo -e "\033[32mconnection state count by port base on client: \033[0m \n"
+    echo -ne "\033[32mconnection state count by port base on client: \033[0m \n"
     awk 'NR>1{sum[$(NF-4),$(NF)]+=1}END{for (key in sum){split(key,subkey,SUBSEP);print subkey[1],subkey[2],sum[subkey[1],subkey[2]]}}' /tmp/ss | sort -k 3 -nr | head -n 10 | awk '{print "\033[50C"$0}'   
     echo   
     #统计端口为80且状态为ESTAB连接数最多的前10个IP
-    echo -e "\033[32mtop 10 ip ESTAB state count at port 80: \033[0m \n"
+    echo -ne "\033[32mtop 10 ip ESTAB state count at port 80: \033[0m \n"
     cat /tmp/ss | grep ESTAB | awk -F'[: ]+' '{sum[$(NF-2)]+=1}END{for (ip in sum){print ip,sum[ip]}}' | sort -k 2 -nr | head -n 10
     #echo -e "############################# \n"
     #统计端口为80且状态为SYN-RECV连接数最多的前10个IP
-    echo -e "\033[32mtop 10 ip SYN-RECV state count at port 80: \033[0m \n"
+    echo -ne "\033[32mtop 10 ip SYN-RECV state count at port 80: \033[0m \n"
     cat /tmp/ss | grep -E "$regSS" | grep SYN-RECV | awk -F'[: ]+' '{sum[$(NF-2)]+=1}END{for (ip in sum){print ip,sum[ip]}}' | sort -k 2 -nr | head -n 10
 }
  
 main(){
     while true; do
-        echo -e "1) real time traffic.\n2) traffic and connection overview.\n"
+        echo -ne "1) real time traffic.\n2) traffic and connection overview.\n"
         read -p "please input your select(ie 1): " select
         case  $select in
             1) realTimeTraffic;break;;
